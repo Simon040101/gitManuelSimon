2a9d3d69803c864fd88edf4a13e10dcbe1669222
Manuel R <reismann.m@gmx.at>
2019-01-29 09:10:47 +0100

    Klasse Person - Enums Gender und Birthmonth hinzugefügt

--- test/test/models/Person.cs
+++ test/test/models/Person.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace test.models
 {
+    enum Gender
+    {
+        male, female, notSpecified
+    }
+
+    enum Birthmonth
+    {
+        january, february, march, april
+    }
     class Person
     {
         int ID { get; set; }
@@ -13,18 +22,22 @@ namespace test.models
         string Lastname { get; set; }
         int Age { get; set; }
         string Department { get; set; }
+        Gender Gender { get; set; }
+        Birthmonth Month { get; set; }
 
-       public Person() : this("", "", 0) { }
-       public Person(string firstname, string lastname, int age)
+        public Person() : this("", "", 0, Gender.notSpecified, Birthmonth.march) { }
+       public Person(string firstname, string lastname, int age, Gender gender, Birthmonth birth)
         {
             this.Firstname = firstname;
             this.Lastname = lastname;
             this.Age = age;
+            this.Gender = gender;
+            this.Month = birth;
         }
 
         public override string ToString()
         {
-            return this.Firstname + "" + this.Lastname + "" + this.Age + "";
+            return this.Firstname + "" + this.Lastname + "" + this.Age + "" + this.Gender + "" + this.Month;
         }
     }
 }
